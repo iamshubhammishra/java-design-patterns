fc6a17e985152d2054b9e4dd2f130ae063fb3ff3
Shubham Mishra <mshubham2002@gmail.com>
2025-05-31 21:37:54 +0530

    Virtual Thread

---
+++ virtualThread/TraditionalThread.java
@@ -0,0 +1,27 @@
+package virtualThread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TraditionalThread {
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        for(int i=0;i<1000000;i++){
+            Thread thread = new Thread(()->{
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            thread.setName("Thread"+i);
+            thread.start();
+            System.out.println("Thread number: "+i);
+            threads.add(thread);
+        }
+        for (Thread thread: threads){
+            thread.join();
+            System.out.println("Completed: "+thread.getName());
+        }
+    }
+}
